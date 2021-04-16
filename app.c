115c6392e6a2d84c05483baf59c613115e885b9b
Jennifer Nadolski <43915837+jnadolski@users.noreply.github.com>
2021-04-16 07:35:58 -0700

    start of UI task code
    msg.data isn't working, idk how to use it as a byte array. are we sending a string?

--- app.c
+++ app.c
@@ -1,3 +1,5 @@
+
+
 #include "app.h"
 #include "edd.h"
 
@@ -26,21 +28,21 @@ static xQueueHandle app_queue_handle;
 static xTaskHandle ui_task_handle;
 static xQueueHandle ui_queue_handle;
 
-void init_app(){
-    for(int i = 0; i < NUM_EVENTS; ++i){
-        if(i%2 == 0){ //TODO: make this more realistic
+void init_app() {
+    for (int i = 0; i < NUM_EVENTS; ++i) {
+        if (i % 2 == 0) { //TODO: make this more realistic
             events[i].event_type = HW_EVT_LONG_PRESS;
         }
-        else{
+        else {
             events[i].event_type = HW_EVT_SHORT_PRESS;
         }
     }
 
-    for(int i = 0; i < NUM_PPG_SAMPLES; ++i){
+    for (int i = 0; i < NUM_PPG_SAMPLES; ++i) {
         ppg_samples[i] = i; //TODO: make this more realistic
     }
 
-    for(int i = 0; i < NUM_IMU_SAMPLES; ++i){
+    for (int i = 0; i < NUM_IMU_SAMPLES; ++i) {
         imu_samples[i] = i; //TODO: make this more realistic
     }
 
@@ -75,110 +77,144 @@ void init_app(){
     ui_task_data.task = ui_task_handle;
 }
 
-void hw_timer_callback(TimerHandle_t xTimer){
-	//Send NEXT_EVENT message to hardware task
+void hw_timer_callback(TimerHandle_t xTimer) {
+    //Send NEXT_EVENT message to hardware task
     task_msg_t msg = {};
     msg.type = HW_NEXT_EVENT;
     xQueueSend(hw_task_handle, &msg, portMAX_DELAY);
 }
 
 // aperiodic
-void hardware_task (void* pvParameters) { //this should have higher priority than rest, but lower priority than scheduler
+void hardware_task(void* pvParameters) { //this should have higher priority than rest, but lower priority than scheduler
     task_msg_t message = {};
     //send first hardware event
     hw_timer_callback(NULL);
-    while(1){
-		xQueueReceive(hw_queue_handle, &message, portMAX_DELAY); //wait for message
-		
-        switch(message.type){
-			case HW_NEXT_EVENT:
-                {//adds local scoping to switch statement
-                    hw_evt_t next_event = events[curr_evt];
-                    curr_evt++;
-                    if(curr_evt >= NUM_EVENTS){
-                        curr_evt = 0; //loop around to beginning
-                    }
-                    TimerHandle_t timer = xTimerCreate("hw_evt_timer", next_event.time_stamp - xTaskGetTickCount(), pdFALSE, (void*)HW_EVT_TIMER, hw_timer_callback); //TODO: make sure this isn't negative, and that void* cast is OK
-                    xTimerStart(timer,0);//TODO: need to delete this timer later
-                
-                    switch(next_event.event_type){
-                        case HW_EVT_SHORT_PRESS:
-                            {
-                            //send message to UI Task
-                            task_msg_t msg = {};
-                            msg.type = UI_SHORT_BUTTON_PRESS;
-                            xQueueSend(ui_task_handle, &msg, portMAX_DELAY);
-                            }
-                            break;
-                        case HW_EVT_LONG_PRESS:
-                            {
-                            //send message to UI Task
-                            task_msg_t msg = {};
-                            msg.type = UI_LONG_BUTTON_PRESS;
-                            xQueueSend(ui_task_handle, &msg, portMAX_DELAY);
-                            }
-                            break;
-                    }
-                }
+    while (1) {
+        xQueueReceive(hw_queue_handle, &message, portMAX_DELAY); //wait for message
+
+        switch (message.type) {
+        case HW_NEXT_EVENT:
+        {//adds local scoping to switch statement
+            hw_evt_t next_event = events[curr_evt];
+            curr_evt++;
+            if (curr_evt >= NUM_EVENTS) {
+                curr_evt = 0; //loop around to beginning
+            }
+            TimerHandle_t timer = xTimerCreate("hw_evt_timer", next_event.time_stamp - xTaskGetTickCount(), pdFALSE, (void*)HW_EVT_TIMER, hw_timer_callback); //TODO: make sure this isn't negative, and that void* cast is OK
+            xTimerStart(timer, 0);//TODO: need to delete this timer later
+
+            switch (next_event.event_type) {
+            case HW_EVT_SHORT_PRESS:
+            {
+                //send message to UI Task
+                task_msg_t msg = {};
+                msg.type = UI_SHORT_BUTTON_PRESS;
+                xQueueSend(ui_task_handle, &msg, portMAX_DELAY);
+            }
             break;
-            case HW_IMU_REQUEST:
-                //send message to Activity Task, data is vector of acceleration magnitudes
-                {
-                    //send message to UI Task
-                    task_msg_t msg = {};
-                    msg.type = ACT_IMU_DATA;
-                    memcpy(msg.data,&imu_samples[curr_imu], sizeof(imu_sample_t));
-                    curr_imu++;
-                    if(curr_imu >= NUM_IMU_SAMPLES){
-                        curr_imu = 0;
-                    }
-                    xQueueSend(act_task_handle, &msg, portMAX_DELAY);
-                }
+            case HW_EVT_LONG_PRESS:
+            {
+                //send message to UI Task
+                task_msg_t msg = {};
+                msg.type = UI_LONG_BUTTON_PRESS;
+                xQueueSend(ui_task_handle, &msg, portMAX_DELAY);
+            }
             break;
-            case HW_PPG_REQUEST:
-                //send message to HR Monitor Task, data is vector of timestamps indicating the peaks in the waveform
-                {
-                    task_msg_t msg = {};
-                    msg.type = HRM_PPG_DATA;
-                    memcpy(msg.data, &ppg_samples[curr_ppg], sizeof(ppg_sample_t));
-                    curr_ppg++;
-                    if(curr_ppg >= NUM_PPG_SAMPLES){
-                        curr_ppg = 0;
-                    }
-                    xQueueSend(hrm_task_handle, &msg, portMAX_DELAY);
-                }
-            break;
-            case HW_SCREEN_UPDATE:
-                //print “screen” to console. “Screen” is just a string sent in the data field of this task
-                printf((char*)message.data);
+            }
+        }
+        break;
+        case HW_IMU_REQUEST:
+            //send message to Activity Task, data is vector of acceleration magnitudes
+        {
+            //send message to UI Task
+            task_msg_t msg = {};
+            msg.type = ACT_IMU_DATA;
+            memcpy(msg.data, &imu_samples[curr_imu], sizeof(imu_sample_t));
+            curr_imu++;
+            if (curr_imu >= NUM_IMU_SAMPLES) {
+                curr_imu = 0;
+            }
+            xQueueSend(act_task_handle, &msg, portMAX_DELAY);
+        }
+        break;
+        case HW_PPG_REQUEST:
+            //send message to HR Monitor Task, data is vector of timestamps indicating the peaks in the waveform
+        {
+            task_msg_t msg = {};
+            msg.type = HRM_PPG_DATA;
+            memcpy(msg.data, &ppg_samples[curr_ppg], sizeof(ppg_sample_t));
+            curr_ppg++;
+            if (curr_ppg >= NUM_PPG_SAMPLES) {
+                curr_ppg = 0;
+            }
+            xQueueSend(hrm_task_handle, &msg, portMAX_DELAY);
+        }
+        break;
+        case HW_SCREEN_UPDATE:
+            //print “screen” to console. “Screen” is just a string sent in the data field of this task
+            printf((char*)message.data);
             break;
         }
     }
 }
 
-void generator_task(void* pvParameters){
-    while(1){
+void generator_task(void* pvParameters) {
+    while (1) {
         // Add each of the tasks to list using task_create 
-        task_create(&act_task_data, "activity", BASE_APP_PRIORITY  );
-        task_create(&ui_task_data, "ui", BASE_APP_PRIORITY  );
-        task_create(&app_task_data, "app", BASE_APP_PRIORITY  );
-        task_create(&hrm_task_data, "hrm", BASE_APP_PRIORITY  ); 
-        vTaskDelete( generator_task_handle ); //delete self
+        task_create(&act_task_data, "activity", BASE_APP_PRIORITY);
+        task_create(&ui_task_data, "ui", BASE_APP_PRIORITY);
+        task_create(&app_task_data, "app", BASE_APP_PRIORITY);
+        task_create(&hrm_task_data, "hrm", BASE_APP_PRIORITY);
+        vTaskDelete(generator_task_handle); //delete self
     }
 }
 
-void activity_task(void* pvParameters){
-    
-}
-
-void hr_monitor_task(void* pvParameters){
+void activity_task(void* pvParameters) {
 
 }
 
-void app_task(void* pvParameters){
+void hr_monitor_task(void* pvParameters) {
 
 }
 
-void ui_task(void* pvParameters){
+void app_task(void* pvParameters) {
 
+}
+
+void ui_task(void* pvParameters) {
+    task_msg_t message = {};
+    task_msg_t msg = {};
+
+    while (1) {
+        xQueueReceive(ui_queue_handle, &message, portMAX_DELAY); //wait for message
+
+        switch (message.type) {
+
+            // hardware events 
+        case UI_SHORT_BUTTON_PRESS:
+            msg.type = HW_SCREEN_UPDATE;
+            //msg.data = (char*) "screen";
+            xQueueSend(hw_task_handle, &msg, portMAX_DELAY);
+            break;
+
+        case UI_LONG_BUTTON_PRESS:
+            msg.type = HW_SCREEN_UPDATE;
+            //msg.data = "screen";
+            xQueueSend(hw_task_handle, &msg, portMAX_DELAY);
+            break;
+
+            // app events 
+        case APP_REMINDER:
+            msg.type = HW_SCREEN_UPDATE;
+            //msg.data = "screen";
+            xQueueSend(hw_task_handle, &msg, portMAX_DELAY);
+            break;
+
+        case APP_HEARTRATE:
+            msg.type = HW_SCREEN_UPDATE;
+            //msg.data = "screen";
+            xQueueSend(hw_task_handle, &msg, portMAX_DELAY);
+            break;
+        }
+    }
 }
