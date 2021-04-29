38a91de81ce8b52b383091b3b66f3899191b8780
Joseph Emnett <jcemnett@gmail.com>
2021-04-28 23:39:07 -0700

    Fixed num of priorities, maybe need to increase

--- FreeRTOSConfig.h
+++ FreeRTOSConfig.h
@@ -72,7 +72,7 @@ configMAX_PRIORITIES - 1. */
 #define configTIMER_QUEUE_LENGTH				20
 #define configTIMER_TASK_STACK_DEPTH			( configMINIMAL_STACK_SIZE * 2 )
 
-#define configMAX_PRIORITIES					( 7 )
+#define configMAX_PRIORITIES					( 20 )
 
 /* Run time stats gathering configuration options. */
 unsigned long ulGetRunTimeCounterValue( void ); /* Prototype of function that returns run time counter. */
